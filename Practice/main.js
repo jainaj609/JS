452a8f22f6da651693e88068b15f608f4ac67029
jainaj609 <jainaj609@gmail.com>
2023-02-01 22:17:44 +0530

    Added Handson 3

---
+++ Practice/main.js
@@ -0,0 +1,15 @@
+// const CompletePattern = (N) => 
+// {
+//   for(let i=1; i<=N; i++){
+//     let s="";
+//     for(let j=3; j<=N-1; j--){
+//       s=s+"#";
+//     }
+//     for(let k=1; k<=i; k++){
+//       s=s+k;
+//     }
+//     console.log(s);
+//   }  
+// };
+
+// CompletePattern(4);

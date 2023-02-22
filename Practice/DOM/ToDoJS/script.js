4e78b4cf691ae53bb129b4b7ab9bd6b6d96a4d9b
jainaj609 <jainaj609@gmail.com>
2023-02-22 21:44:29 +0530

    Completed Handson Guess The Number

--- Practice/DOM/ToDoJS/script.js
+++ Practice/DOM/ToDoJS/script.js
@@ -1,19 +1,15 @@
 document.getElementById("createList").addEventListener('click', createList);
 document.getElementById("close").addEventListener('click', removeListForm);
 document.getElementById("addList").addEventListener('click', createCard);
-
+let cards = [];
 function createList() {
     console.log("List created");
-
     document.getElementsByClassName("listForm")[0].style.display = "block";
-    // document.getElementById("listForm").style.display = "block";
 }
 
 function removeListForm() {
     console.log("List closed");
-
     document.getElementsByClassName("listForm")[0].style.display = "none";
-
 }
 
 function createCard() {
@@ -23,15 +19,35 @@ function createCard() {
     let title = document.createElement("h4");
     title.innerText = document.getElementById("listName").value;// read the value from the form
 
-
     let icon = document.createElement("i");
     icon.className = "fa-solid fa-plus";
 
+    let del = document.createElement("i");
+    del.className = "fa fa-trash";
+
     let list = document.createElement("div");
-    list.append(title, icon);
+    list.append(title, del, icon);
 
-    let lists= document.getElementById("lists");
-    lists.appendChild(list);
+    del.addEventListener("click", function removeElement() {
+        let tempArray = [];
+        for (let i = 0; i < cards.length; i++) {
+            if (cards[i] !== list)
+                tempArray.push(cards[i]);
+        }
+        cards = tempArray;
+        display(cards);
+    });
 
+    cards.push(list);
+    display(cards);
+
+
+    function display(n) {
+        let lists = document.getElementById("lists");
+        lists.innerHTML = "";
+        for (let i=0; i<n.length; i++){
+            lists.appendChild(n[i]);
+        }
+    }
 
 }
